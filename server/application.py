759a11fbda639776fcb8be6895be2f82d739d319
fin1kk <113370280+fin1kk@users.noreply.github.com>
2025-12-07 16:09:33 +0500

    Update application.py

--- server/application.py
+++ server/application.py
@@ -1,16 +1,17 @@
 import http.server
 import socketserver
+
 PORT = 8000
 
-classTestMe():
-  def take_five(self):
-    return 4
-  def port(self):
-    return PORT
-    
+class TestMe:
+    def take_five(self):
+        return 4
+    def port(self):
+        return PORT
+
 if __name__ == '__main__':
-  Handler = http.server.SimpleHTTPRequestHandler
-  
-  with socketserver.TCPServer(("",PORT),Handler) as http:
-    print("servingatport",PORT)
-    http.serve_forever()
+    Handler = http.server.SimpleHTTPRequestHandler
+
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
